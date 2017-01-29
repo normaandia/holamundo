e7b72308761474f0ed68bfbb634083aeb35a38ca
normaandia <imasumac_israely@hotmail.com>
2017-01-29 13:08:20 -0500

    Add files via upload

---
+++ formulario.php
@@ -0,0 +1,12 @@
+formulario php
+<html>
+<head>
+</head>
+<body>
+<form action = " proceso.php" methed = "post">
+<p> su nombre<input type = "text" name ="nombre"/></?>
+<p> su edad<input type = "text" name ="edad"/></?>
+<p> enviar<input type = "submit" name ="enviar"/></?>
+</form>
+</body>
+</html>
