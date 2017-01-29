d32d22e37f8c71ae52164e4e17a08a132092b679
normaandia <imasumac_israely@hotmail.com>
2017-01-29 13:10:36 -0500

    Add files via upload

---
+++ proceso.php
@@ -0,0 +1,16 @@
+proceso.php
+<?php
+$nom = $-post{´nombre´};
+$e = $-post{´edad´};
+?>
+<htm>
+<head></head>
+<body>
+<p>
+<?php
+echo"su nombre es".$nom."<br/>";
+echo"su edad es".$e."<br/>";
+?>
+</p>
+</body>
+</html>
